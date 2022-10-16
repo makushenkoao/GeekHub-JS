6972155d88fbfc1d08a4389af964802912cdc15a
Anton <108577345+makushenkoao@users.noreply.github.com>
2022-10-16 23:46:36 +0300

    Second commit

--- homework-1-1/main.js
+++ homework-1-1/main.js
@@ -3,23 +3,24 @@ const pi = Math.PI;
 console.log(pi.toFixed(2));
 // task 2.2
 const result = 0.6 + 0.7;
-console.log(Math.round(result));
+console.log(result.toFixed(1));
 // task 2.3
 const $ = '100$';
 console.log(parseInt($));
 // task 3.1
 const str = 'some test string';
-console.log(`${str[0]} ${str[str.length - 1]}`);
+console.log(`${str[0]},${str[str.length - 1]}`);
 // task 3.2
-console.log(`${str[0].toUpperCase()}ome test strin${str[str.length -1].toUpperCase()}`);
+const newStr = str[0].toUpperCase() + str.slice(1, -1) + str[str.length - 1].toUpperCase()
+console.log(newStr)
 // task 3.3
-console.log(str.lastIndexOf(' '));
+console.log(str.indexOf(' ', str.indexOf(' ') + 1))
 // task 3.4
-console.log(str.substring(0, 9));
+console.log(str.slice(0, -6));
 // task 4
-let searchFlat = (num) => {
+const getEntranceByFlatNumber = (num) => {
     if (num > 0 && num <= 20) return 1;
     else if (num > 20 && num <= 40) return 2;
     else if (num > 40 && num <= 60) return 3;
 }
-console.log(searchFlat(12));
+console.log(getEntranceByFlatNumber(12));
